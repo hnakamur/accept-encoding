5413cf3eef5bf75c7197226fffbec512c3636609
Hiroaki Nakamura <hnakamur@gmail.com>
2023-04-23 05:44:39 +0900

    Benchmark is_ascii_lowercase
    ```
    is_ascii_lowercase/stdlib
                            time:   [74.250 ns 74.257 ns 74.263 ns]
    Found 12 outliers among 100 measurements (12.00%)
      2 (2.00%) low mild
      6 (6.00%) high mild
      4 (4.00%) high severe
    is_ascii_lowercase/contains
                            time:   [74.263 ns 74.386 ns 74.655 ns]
    Found 7 outliers among 100 measurements (7.00%)
      3 (3.00%) high mild
      4 (4.00%) high severe
    is_ascii_lowercase/primitive
                            time:   [75.865 ns 77.210 ns 78.738 ns]
    Found 18 outliers among 100 measurements (18.00%)
      18 (18.00%) high severe
    ```

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -12,5 +12,45 @@ fn bench_fibs(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, bench_fibs);
+#[inline]
+fn is_ascii_lowercase_stdlib(c: u8) -> bool {
+    c.is_ascii_lowercase()
+}
+
+#[inline]
+fn is_ascii_lowercase_contains(c: u8) -> bool {
+    (b'a'..=b'z').contains(&c)
+}
+
+#[inline]
+fn is_ascii_lowercase_primitive(c: u8) -> bool {
+    b'a' <= c && c <= b'z'
+}
+
+fn bench_is_ascii_lowercase(c: &mut Criterion) {
+    let mut group = c.benchmark_group("is_ascii_lowercase");
+    group.bench_function("stdlib", |b| {
+        b.iter(|| {
+            for c in b'\x00'..=b'\xFF' {
+                black_box(is_ascii_lowercase_stdlib(c));
+            }
+        })
+    });
+    group.bench_function("contains", |b| {
+        b.iter(|| {
+            for c in b'\x00'..=b'\xFF' {
+                black_box(is_ascii_lowercase_contains(c));
+            }
+        })
+    });
+    group.bench_function("primitive", |b| {
+        b.iter(|| {
+            for c in b'\x00'..=b'\xFF' {
+                black_box(is_ascii_lowercase_primitive(c));
+            }
+        })
+    });
+}
+
+criterion_group!(benches, bench_is_ascii_lowercase);
 criterion_main!(benches);
