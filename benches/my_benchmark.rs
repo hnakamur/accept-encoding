f24f47aaf40bfdd3bc707df9f9ecd24cc712e0a3
Hiroaki Nakamura <hnakamur@gmail.com>
2023-04-22 22:04:46 +0900

    Benchmark is_tchar between table-based and match-based implementations
    ```
    is_tchar/table/0        time:   [8.0200 ns 8.1449 ns 8.2605 ns]
    Found 23 outliers among 100 measurements (23.00%)
      2 (2.00%) high mild
      21 (21.00%) high severe
    is_tchar/match/0        time:   [18.958 ns 18.974 ns 18.992 ns]
    Found 4 outliers among 100 measurements (4.00%)
      4 (4.00%) high mild
    is_tchar/table/1        time:   [6.0052 ns 6.0059 ns 6.0070 ns]
    Found 7 outliers among 100 measurements (7.00%)
      1 (1.00%) low mild
      2 (2.00%) high mild
      4 (4.00%) high severe
    is_tchar/match/1        time:   [14.676 ns 14.694 ns 14.713 ns]
    Found 3 outliers among 100 measurements (3.00%)
      1 (1.00%) high mild
      2 (2.00%) high severe
    ```

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -12,5 +12,80 @@ fn bench_fibs(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, bench_fibs);
+#[rustfmt::skip]
+const TCHAR_TABLE: [u8; 256] = [
+    // tchar = "!" / "#" / "$" / "%" / "&" / "'" / "*" / "+" / "-" / "." /
+    //         "^" / "_" / "`" / "|" / "~" / DIGIT / ALPHA
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+#[inline]
+fn is_tchar_with_table(c: u8) -> bool {
+    TCHAR_TABLE[c as usize] != 0
+}
+
+#[inline]
+fn is_tchar_with_match(c: u8) -> bool {
+    match c {
+        // token = 1*tchar
+        // tchar = "!" / "#" / "$" / "%" / "&" / "'" / "*" / "+" / "-" / "." /
+        //         "^" / "_" / "`" / "|" / "~" / DIGIT / ALPHA
+        b'!'
+        | b'#'
+        | b'$'
+        | b'%'
+        | b'&'
+        | b'\''
+        | b'*'
+        | b'+'
+        | b'-'
+        | b'.'
+        | b'^'
+        | b'_'
+        | b'`'
+        | b'|'
+        | b'~'
+        | b'0'..=b'9'
+        | b'A'..=b'Z'
+        | b'a'..=b'z' => true,
+        _ => false,
+    }
+}
+
+fn bench_is_tchar(c: &mut Criterion) {
+    let mut group = c.benchmark_group("is_tchar");
+    let input_values: Vec<&[u8]> = vec![b"gzip, deflate, br", b"gzip, deflate"];
+    for i in 0..input_values.len() {
+        group.bench_with_input(BenchmarkId::new("table", i), &i, |b, i| {
+            b.iter(|| {
+                for c in input_values[*i].iter() {
+                    black_box(is_tchar_with_table(*c));
+                }
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("match", i), &i, |b, i| {
+            b.iter(|| {
+                for c in input_values[*i].iter() {
+                    black_box(is_tchar_with_match(*c));
+                }
+            })
+        });
+    }
+}
+criterion_group!(benches, bench_is_tchar);
 criterion_main!(benches);
